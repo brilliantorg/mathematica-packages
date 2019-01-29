d2bc5d0ff26678056a534be6beb6f71c19f85ae1
joshmaxsilverman <josh.silverman@gmail.com>
2019-01-28 22:27:21 -0800

    Mathematica init file.

---
+++ init.m
@@ -0,0 +1,40 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*SetDirectory@NotebookDirectory[];*)
+
+
+(* ::Input:: *)
+(*BrillBlue=RGBColor["#3D99F6"];*)
+(*BrillOrange=RGBColor["#EC7300"];*)
+(*BrillRed=RGBColor["#D61F06"];*)
+(*BrillGray=RGBColor["#333333"];*)
+(*BrillGreen=RGBColor["#67D94C"];*)
+(*BrillPurple=RGBColor["#BA33D6"];*)
+(*BrillYellow=RGBColor["#F2C94C"];*)
+(*PTGreen=RGBColor["#20A900"];*)
+(*PBWhite=RGBColor["#FFF7E6"];*)
+(*DarkBlue=RGBColor["#625CDB"];*)
+(*DDarkBlue=RGBColor["#302B94"];*)
+(*BrDBlue=RGBColor["#0C6AC7"];*)
+(*hexToRGB=(IntegerDigits[ToExpression@StringReplace[#,"#"->"16^^"],256,3]/255.)&;*)
+
+
+(* ::Input:: *)
+(*PlRed=RGBColor["#FF664D"];*)
+(*PlPurple=RGBColor["#69047E"];*)
+(*PBrown=RGBColor["#B4825D"];*)
+
+
+(* ::Input:: *)
+(*COrange=RGBColor["#FCA04A"];*)
+(*DCol=RGBColor["#777777"];*)
+
+
+(* ::Input:: *)
+(*Needs["MaTeX`"];*)
+(*ConfigureMaTeX[];*)
+
+
+(* ::Input:: *)
+(*SetOptions[MaTeX,"Preamble"->{"\\usepackage{color}\\definecolor{bred}{rgb}"<>ToString[hexToRGB["#D61F06"]]<>"\\definecolor{bgreen}{rgb}"<>ToString[hexToRGB["#20A900"]]<>"\\definecolor{bblue}{rgb}"<>ToString[hexToRGB["#0C6AC7"]]<>"\\definecolor{bdblue}{rgb}"<>ToString[hexToRGB["#302B94"]]<>"\\definecolor{purp}{rgb}"<>ToString[hexToRGB["#69047E"]]<>"\\definecolor{pred}{rgb}"<>ToString[hexToRGB["#FF664D"]]<>"\\definecolor{bgray}{rgb}"<>ToString[hexToRGB["#333333"]]}];*)
